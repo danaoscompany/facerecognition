67ad6400df347f838f0150b88380309b8238712e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-12-28 00:19:23 +0700

    Last commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ wss.on('connection', ws => {
 		console.log(img1url);
 		console.log(img2url);
 		// python2 main.py --img1url http://45.132.240.102/facerecognition/img1.jpg --img2url http://45.132.240.102/facerecognition/img2.jpg
-		exec("python2 /home/dana/Documents/Web/facerecognition/main.py --img1url "+img1url+" --img2url "+img2url, (error, stdout, stderr) => {
+		exec("python2 main.py --img1url "+img1url+" --img2url "+img2url, (error, stdout, stderr) => {
 		    if (error) {
 		        console.log(`error: ${error.message}`);
 		        return;
