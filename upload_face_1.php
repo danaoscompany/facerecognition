bc29d2c547b56d2ffced1f2f51efb663452a305a
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-12-28 01:22:17 +0700

    Last commit

--- upload_face_1.php
+++ upload_face_1.php
@@ -1,4 +1,2 @@
 <?php
-if (isset($_FILES['file'])) {
-	move_uploaded_file($_FILES['file']['tmp_name'], './face1.jpg');
-}
+move_uploaded_file($_FILES['file']['tmp_name'], './face1.jpg');
--- upload_face_2.php
+++ upload_face_2.php
@@ -1,4 +1,2 @@
 <?php
-if (isset($_FILES['file'])) {
-	move_uploaded_file($_FILES['file']['tmp_name'], './face2.jpg');
-}
+move_uploaded_file($_FILES['file']['tmp_name'], './face2.jpg');
